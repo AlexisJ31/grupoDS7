b66d7fbfa41973f9024debbf40152be411bb3663
Alexis <alexisjaen35@gmail.com>
2026-04-16 01:01:56 -0500

    ecommerse lite

---
+++ modulo1_poo/lab3/ecommerce lite/js/main.js
@@ -0,0 +1,7 @@
+// ===== MAIN JS BÁSICO =====
+
+// Función para abrir/cerrar el menú en teléfonos móviles
+function toggleMenu() {
+  const mobileMenu = document.getElementById('mobileMenu');
+  mobileMenu.classList.toggle('open');
+}
